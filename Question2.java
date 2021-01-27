b589eab86eeec86259dc1f1f628cbeb6fc6f21da
ANSH SAXENA <60190254+anshsaxena5621@users.noreply.github.com>
2021-01-27 13:44:05 +0530

    Initial commit

---
+++ Question2.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Question2 {
+    public static final int MAX_UNICODE = 65_535;
+
+    public static void main(String[] args) {
+        System.out.println(checkWhetherStringIsUnique("Competitive Programming"));
+    }
+
+    public static boolean checkWhetherStringIsUnique(String string) {
+        // create a variable of boolean type
+        boolean result = false;
+
+
+
+
+        Map<Character, Boolean> characters = new HashMap<>();
+
+        // Write your code here...
+
+        return result;
+    }
+}
